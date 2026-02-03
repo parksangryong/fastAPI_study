a791de1e6bbccb80bf8523c1b94014a47be78474
parksangryong <ppsr3478@naver.com>
2026-02-03 17:26:26 +0900

    api 연결

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # main.py 생성
 from fastapi import FastAPI
+from user.interface.user_controller import router as user_router
 
 app = FastAPI()
+
+app.include_router(user_router)
 
 @app.get("/")
 def read_root():
