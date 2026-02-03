d3a271c57b69e892ffa572ad7fcd5ccde13d9b97
parksangryong <ppsr3478@naver.com>
2026-02-03 17:00:31 +0900

    settings

---
+++ user/domain/repository/user_repo.py
@@ -0,0 +1,11 @@
+from abc import ABCMeta, abstractmethod
+from user.domain.user import User
+
+class IUserRepository(metaclass=ABCMeta):
+    @abstractmethod
+    def save(self, user: User):
+        raise NotImplementedError
+
+    @abstractmethod
+    def find_by_email(self, email: str) -> User:
+        raise NotImplementedError
